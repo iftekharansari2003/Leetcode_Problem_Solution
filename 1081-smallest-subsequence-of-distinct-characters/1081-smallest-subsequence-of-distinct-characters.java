02014f3a4882c4f1a3c4ddb4fafa7ba24901274e
Iftekhar_Ansari <143179577+iftekharansari2003@users.noreply.github.com>
2026-07-19 16:17:56 +0530

    Time: 3 ms (71.44%), Space: 43.1 MB (15.88%) - LeetHub

---
+++ 1081-smallest-subsequence-of-distinct-characters/1081-smallest-subsequence-of-distinct-characters.java
@@ -0,0 +1,31 @@
+class Solution {
+    public String smallestSubsequence(String s) {
+        boolean isVisited[]= new boolean[26];
+        int lastidx[] =new int[26];
+
+        for(int i=0;i<s.length();i++){
+            lastidx[s.charAt(i)-'a']=i;
+        }
+
+        Stack<Character> st =new Stack<>();
+
+        st.push(s.charAt(0));
+        isVisited[s.charAt(0)-'a']=true;
+
+        for(int i=1;i<s.length();i++){
+            if(!isVisited[s.charAt(i)-'a']){
+                while(!st.isEmpty() && (st.peek()-'a'>s.charAt(i)-'a') && lastidx[st.peek()-'a']>i){
+                    isVisited[st.pop()-'a']=false;
+                }
+                st.push(s.charAt(i));
+                isVisited[s.charAt(i)-'a']=true;
+            }
+        }
+        StringBuilder sb=new StringBuilder("");
+        while(!st.isEmpty()){
+            sb.append(st.peek());
+            st.pop();
+        }
+        return sb.reverse().toString();
+    }
+}
