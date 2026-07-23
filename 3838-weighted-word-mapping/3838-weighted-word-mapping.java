c8e0ac147984f401ed77f3cac321df96f846a736
Iftekhar_Ansari <143179577+iftekharansari2003@users.noreply.github.com>
2026-07-23 15:38:27 +0530

    Time: 2 ms (93.33%), Space: 46.7 MB (30.83%) - LeetHub

--- 3838-weighted-word-mapping/3838-weighted-word-mapping.java
+++ 3838-weighted-word-mapping/3838-weighted-word-mapping.java
@@ -1,20 +1,14 @@
 class Solution {
     public String mapWordWeights(String[] words, int[] weights) {
         StringBuilder sb=new StringBuilder("");
-
-        char arr[]=new char[26];
-        int p=25;
-        for(char i='a';i<='z';i++){
-            arr[p--]=i;
-        }
-
         for(int i=0;i<words.length;i++){
             String word=words[i];
             int weight=0;
             for(int j=0;j<word.length();j++){
                 weight+=weights[word.charAt(j)-'a'];
             }
-            sb.append(arr[weight%26]);
+            char ch = (char) ('z' - (weight % 26));
+            sb.append(ch);
         }
         
         return sb.toString();
