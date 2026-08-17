0233b721056eb8f7b08bd1ff25acdc0834e3360b
Iftekhar_Ansari <143179577+iftekharansari2003@users.noreply.github.com>
2026-08-17 14:32:37 +0530

    Time: 585 ms (63.99%), Space: 0B (100%) - LeetHub

--- 0183-customers-who-never-order/0183-customers-who-never-order.sql
+++ 0183-customers-who-never-order/0183-customers-who-never-order.sql
@@ -1,2 +1,8 @@
 # Write your MySQL query statement below
-Select c.name as Customers from Customers as c Left Join Orders as O On c.id = O.customerId where o.customerId is null;
+SELECT c.name AS Customers
+FROM Customers c
+WHERE NOT EXISTS (
+    SELECT 1
+    FROM Orders o
+    WHERE o.customerId = c.id
+);
