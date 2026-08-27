0c8a6fdefe3123744ac97a3b807a779f60dbcf42
Iftekhar_Ansari <143179577+iftekharansari2003@users.noreply.github.com>
2026-08-27 22:23:47 +0530

    Time: 3 ms (59.26%), Space: 52.9 MB (10.47%) - LeetHub

--- 0485-max-consecutive-ones/0485-max-consecutive-ones.java
+++ 0485-max-consecutive-ones/0485-max-consecutive-ones.java
@@ -5,12 +5,12 @@ class Solution {
       for(int i=0;i<nums.length;i++){
         if(nums[i]==1){
             count++;
-        }else{
             ans=Math.max(ans,count);
+        }else{
             count=0;
         }
       }
-      ans=Math.max(ans,count);
+
       return ans;
     }
 }
